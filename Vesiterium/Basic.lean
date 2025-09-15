a5f02838a0c75256094a9897974260287f250598
pingu <nor@acorneroftheweb.com>
2025-09-15 10:35:59 +0200

    ow

--- Vesiterium/Basic.lean
+++ Vesiterium/Basic.lean
@@ -6,7 +6,21 @@ structure Pair (A : Type u) (B : Type v) where
    fst : A
    snd : B
 
-theorem mul_le_self (a b c : Nat) (p : a ≤ b) : a * c ≤ b * c := Nat.mul_le_mul p (.refl)
+theorem mul_le_self {a b c : Nat} (p : a ≤ b) : a * c ≤ b * c := Nat.mul_le_mul p (.refl)
 
-def pairByteToWord : Pair Byte Byte → Word
-  | .mk ⟨v₁, p₁⟩ ⟨v₂, p₂⟩ => ⟨ (v₁ * 256) + v₂, Nat.add_le_add (mul_le_self v₁ 255 256 p₁) p₂ ⟩
+theorem WordDivision256 { v : Word } : v.val / 256 ≤ 255 := by
+  have ⟨ val, p ⟩ := v
+  apply Nat.div_le_of_le_mul
+  sorry
+
+
+theorem mod_succ_le {a b : Nat} : (a % b.succ) ≤ b :=
+  Nat.lt_succ.mp (Nat.mod_lt a (Nat.zero_lt_succ b) )
+
+def PairByteToWord : Pair Byte Byte → Word
+  | .mk ⟨v₁, p₁⟩ ⟨v₂, p₂⟩ => ⟨ (v₁ * 256) + v₂, Nat.add_le_add (mul_le_self p₁) p₂ ⟩
+
+def WordToPairByte : Word → Pair Byte Byte
+  | w@(⟨ v, _⟩) => .mk
+    ⟨ w.val / 256 , WordDivision256 ⟩
+    ⟨ v % 256 , mod_succ_le ⟩
